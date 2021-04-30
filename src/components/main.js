35f9a7c7e76fa906029bbcca323ea0e78f3cbebb
ethanvachon <ethanhvachon@gmail.com>
2021-04-29 22:58:37 -0600

    basic wiring

--- src/components/main.js
+++ src/components/main.js
@@ -3,24 +3,15 @@ import './main.css'
 
 function Button(props) {
   return (
-    <button className={"bg-white flex justify-center rounded-full p-5 button text-white text-3xl " + props.className}>
+    <button className={"bg-white flex justify-center rounded-full p-5 button text-white text-3xl " + props.className} onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
 
-class Display extends React.Component{
-  render() {
-    return (
-      <div className="bg-white rounded position">
-        test
-      </div>
-    );
-  }
-}
 class Buttons extends React.Component {
   renderButton(i, color) {
-    return <Button value={i} className={color}/>
+    return <Button value={i} className={color} onClick={() => this.props.onClick(i)} />
   }
   render() {
     return (
@@ -32,25 +23,25 @@ class Buttons extends React.Component {
           {this.renderButton("÷", "bg-yellow-500 hover:bg-yellow-600")}
         </div>
         <div className="flex justify-around my-5">
-          {this.renderButton("7", "bg-gray-600 hover:bg-gray-700")}
-          {this.renderButton("8", "bg-gray-600 hover:bg-gray-700")}
-          {this.renderButton("9", "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(7, "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(8, "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(9, "bg-gray-600 hover:bg-gray-700")}
           {this.renderButton("x", "bg-yellow-500 hover:bg-yellow-600")}
         </div>
         <div className="flex justify-around my-5">
-          {this.renderButton("4", "bg-gray-600 hover:bg-gray-700")}
-          {this.renderButton("5", "bg-gray-600 hover:bg-gray-700")}
-          {this.renderButton("6", "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(4, "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(5, "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(6, "bg-gray-600 hover:bg-gray-700")}
           {this.renderButton("-", "bg-yellow-500 hover:bg-yellow-600")}
         </div>
         <div className="flex justify-around my-5">
-          {this.renderButton("1", "bg-gray-600 hover:bg-gray-700")}
-          {this.renderButton("2", "bg-gray-600 hover:bg-gray-700")}
-          {this.renderButton("3", "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(1, "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(2, "bg-gray-600 hover:bg-gray-700")}
+          {this.renderButton(3, "bg-gray-600 hover:bg-gray-700")}
           {this.renderButton("+", "bg-yellow-500 hover:bg-yellow-600")}
         </div>
         <div className="flex justify-around my-5">
-          {this.renderButton("0", "bg-gray-600 hover:bg-gray-700 zero")}
+          {this.renderButton(0, "bg-gray-600 hover:bg-gray-700 zero")}
           {this.renderButton(".", "bg-gray-600 hover:bg-gray-700")}
           {this.renderButton("=", "bg-yellow-500 hover:bg-yellow-600")}
         </div>
@@ -59,15 +50,28 @@ class Buttons extends React.Component {
   }
 }
 class Calculator extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDisplay: 0,
+    };
+  }
+  handleClick(num) {
+    if (typeof(num) == 'number') {
+      this.setState({ currentDisplay: num })
+    }
+  }
   render() {
     return(
       <div className="h-screen flex items-center justify-center">
         <div className="custom-sizing bg-black rounded">
           <div className="mt-8 mb-4 flex justify-center">
-            <Display />
+            <div className="bg-white rounded position flex items-center text-8xl px-4">
+              { this.state.currentDisplay}
+            </div>
           </div>
           <div className="px-2">
-            <Buttons />
+            <Buttons onClick={(i) => this.handleClick(i)}/>
           </div>
         </div>
       </div>
