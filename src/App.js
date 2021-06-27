70b21b18219688a924e8d1bd11fa91f80ff2c4e9
ethanvachon <ethanhvachon@gmail.com>
2021-06-27 10:46:39 -0600

    game added, routing, and conditional class render on game info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
 import Calculator from './components/calculator/main'
+import Game from "./components/tic-tac-toe/main";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App bg-gray-900">
-      <Calculator />
-    </div>
+    <Router>
+
+      <nav>
+        <Link to="/calculator">Calculator</Link>
+        <Link to="/game">Tic-Tac-Toe</Link>
+      </nav>
+      <Switch>
+        <Route path="/calculator">
+        <div className="App bg-gray-900">
+          <Calculator />
+        </div>
+        </Route>
+      </Switch>
+      <Switch>
+        <Game />
+      </Switch>
+      <Switch>
+        
+      </Switch>
+     </Router>
   );
 }
 
---
+++ src/components/tic-tac-toe/main.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import './style.css';
+import { withRouter } from 'react-router-dom'
+
+
+function Square(props) {
+    return (
+      <button className="square text-white bg-blue-700" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+  
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <div key={move}>
+          <button className="bg-blue-400 p-2 my-2 text-white rounded hover:bg-blue-500" onClick={() => this.jumpTo(move)}>{desc}</button>
+        </div>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div className="game items-center justify-center h-screen bg-blue-700">
+        <div className="">
+          <h1 className="text-3xl text-center pb-5 text-white">{status}</h1>
+          <div className="game-board">
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+        </div>
+        <div className={ this.props.location.pathname === "/game" ? 'game-info'  : ''}>
+          <div>{moves}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game
